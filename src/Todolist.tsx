af566777a22be474d04ea23c668018ff075a3514
Ruban Pavel <ruban.pavel@outlook.com>
2022-09-14 17:24:36 +0300

    lesson01

---
+++ src/Todolist.tsx
@@ -0,0 +1,39 @@
+import React from "react";
+
+
+type TodoListProps = {
+    title: string,
+    pomid?: number,
+    tasks: Array<TasksType>,
+}
+
+type TasksType = {
+    id: number,
+    title: string,
+    isDone: boolean,
+}
+
+
+export const Todolist = (props: TodoListProps) => {
+    return (
+        <div>
+            <h3>{props.title}</h3>
+            <div>
+                <input/>
+                <button>+{props.pomid}</button>
+            </div>
+            <ul>
+                <li><input type="checkbox" checked={props.tasks[0].isDone}/> <span>{props.tasks[0].title}</span></li>
+                <li><input type="checkbox" checked={props.tasks[1].isDone}/> <span>{props.tasks[1].title}</span></li>
+                <li><input type="checkbox" checked={props.tasks[2].isDone}/> <span>{props.tasks[2].title}</span></li>
+
+
+            </ul>
+            <div>
+                <button>All</button>
+                <button>Active</button>
+                <button>Completed</button>
+            </div>
+        </div>
+    )
+}
